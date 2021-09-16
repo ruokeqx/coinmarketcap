664abe454bdd65046a7b2d0f08de8ded0cf74605
qfrost <root@qfrost.com>
2021-09-16 15:58:57 +0800

    spider

--- spider.go
+++ spider.go
@@ -14,12 +14,52 @@ var market_url = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/marke
 var historical_url = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/historical?id=%d&convertId=2787&timeStart=1626393600&timeEnd=1631750400"
 
 type Coin struct {
-	name  string
-	price string
+	name       string
+	symbol     string
+	timeOpen   string
+	timeClose  string
+	timeHigh   string
+	timeLow    string
+	openPrice  float64
+	highPrice  float64
+	lowPrice   float64
+	closePrice float64
+	volume     float64
+	marketCap  float64
+}
+
+type HistoryData struct {
+	data struct {
+		id     int
+		name   string
+		symbol string
+		quotes []struct {
+			timeOpen  string
+			timeClose string
+			timeHigh  string
+			timeLow   string
+			quote     struct {
+				open      float64
+				high      float64
+				low       float64
+				close     float64
+				volume    float64
+				marketCap float64
+				timestamp float64
+			}
+		}
+	}
+	status struct {
+		timestamp     string
+		error_code    string
+		error_message string
+		elapsed       string
+		credit_count  int
+	}
 }
 
 var jar, err = cookiejar.New(nil) // 设置全局cookie管理器
-func download(url string) {
+func download(url string) []byte {
 	println(url)
 	client := &http.Client{
 		Jar:     jar,              // Jar 域自动管理Cookie
@@ -31,22 +71,56 @@ func download(url string) {
 	res, err := client.Do(req)
 	if err != nil {
 		log.Fatal("http Get failed!")
-		return
+		return nil
 	}
 	defer res.Body.Close() // 注册关闭连接
 	if res.StatusCode != 200 {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("http read body failed!")
+		return nil
+	}
+	// fmt.Println(string(body))
+	return body
+}
 
-	var result interface{}
+func GetId(body []byte) int {
+	// 手动解析json
+	result := make(map[string]interface{})
 	json.Unmarshal(body, &result)
+	id := result["data"].(map[string]interface{})["id"]
+	// fmt.Printf("%v", id)
+	return int(id.(float64)) // 对interface{float64}转化为int
 
-	m := result.(map[string]interface{})
-	println(m)
+}
+
+func ParserHistoryData(body []byte) {
+	// jsonObj, _ := objx.FromJSON(string(body))
+	// quotes := jsonObj.Get("data.quotes").Data()
+	// l := reflect.ValueOf(quotes)
+	// for i := 0; i < l.Len(); i++ {
+	// 	println(l[i])
+	// }
+
+	// result := make(map[string]interface{})
+	// json.Unmarshal(body, &result)
+	// quotes := result["data"].(map[string]interface{})["quotes"]
+	// fmt.Printf("%T", quotes)
+	// println
+
+	var m HistoryData
+	if err := json.Unmarshal([]byte(body), &m); err != nil {
+		panic(err)
+	}
+
+	println(m.data.name)
 }
 
 func main() {
 	url := fmt.Sprintf(market_url, "Bitcoin")
-	download(url)
+	id := GetId(download(url))
+	url = fmt.Sprintf(historical_url, id)
+	ParserHistoryData(download(url))
 }
