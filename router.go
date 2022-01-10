4c9595e4791ba4a454198c7b4002a59cfc38ba99
ruokeqx <ruokeqx@163.com>
2022-01-10 22:32:19 +0800

    page temp

--- router.go
+++ router.go
@@ -366,3 +366,154 @@ func TokenAuthMiddleware(c *gin.Context) {
 	// Pass on to the next-in-chain
 	c.Abort()
 }
+
+/*
+++++++++++++++
++    page    +
+++++++++++++++
+*/
+
+// example //
+// func getgoods(c *gin.Context) {
+// 	var good []goods
+
+// 	name := c.Query("name")
+// 	pages, _ := strconv.Atoi(c.Query("pages"))
+// 	limits, _ := strconv.Atoi(c.Query("limits"))
+// 	db := sqlInit()
+
+// 	if !db.HasTable("goods") {
+// 		db.Table("goods").CreateTable(&goods{})
+// 	}
+// 	db.Table("goods").Where("name Like ?", "%"+name+"%").Offset((pages - 1) * limits).Limit(limits).Find(&good)
+// 	if len(good) == 0 {
+// 		c.JSON(http.StatusBadRequest, gin.H{
+// 			"code": http.StatusBadRequest,
+// 			"msg":  "No data now!",
+// 		})
+// 		return
+// 	} else {
+// 		b, err := json.Marshal(good)
+// 		if err != nil {
+// 			fmt.Println("query result convert to json error!")
+// 			return
+// 		} else {
+// 			c.Writer.Write(b)
+// 			return
+// 		}
+// 	}
+// }
+
+// data to paint chart
+func chart_page(c *gin.Context) {
+	// /data-api/v3/cryptocurrency/detail/chart?coinName=(?)&range=(?)&convertId=(?)
+	db := sqlInit()
+	coinName := c.Query("coinName")
+	if !db.HasTable("chart-" + coinName) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": http.StatusBadRequest,
+			"msg":  "No query coin!",
+		})
+		return
+	}
+	queryRange := c.Query("range")
+	convertId := c.Query("convertId")
+	fmt.Println(convertId)
+	var tE int64
+	var tS int64
+	var chart []CoinPointQuote
+	var retChart []CoinPointQuote
+	switch queryRange {
+	case "1D":
+		// fmt.Println("1D")
+		tE = time.Now().Unix()
+		tS = tE - 60*60*24
+		db.Table("chart-"+coinName).Where("time BETWEEN ? AND ?", tS, tE).Order("time").Find(&chart)
+		retChart = chart
+	case "7D":
+		// fmt.Println("7D")
+		tE = time.Now().Unix()
+		tS = tE - 60*60*24*7
+		db.Table("chart-"+coinName).Where("time BETWEEN ? AND ?", tS, tE).Order("time").Find(&chart)
+		retChart = chart
+	case "1M":
+		// fmt.Println("1M")
+		tE = time.Now().Unix()
+		tS = tE - 60*60*24*30
+		db.Table("chart-"+coinName).Where("time BETWEEN ? AND ?", tS, tE).Order("time").Find(&chart)
+		for index, cont := range chart {
+			if index%12 == 0 {
+				retChart = append(retChart, cont)
+			}
+		}
+	case "1Y":
+		// fmt.Println("1Y")
+		tE = time.Now().Unix()
+		tS = tE - 60*60*24*365
+		db.Table("chart-"+coinName).Where("time BETWEEN ? AND ?", tS, tE).Order("time").Find(&chart)
+		for index, cont := range chart {
+			if index%288 == 0 {
+				retChart = append(retChart, cont)
+			}
+		}
+	default:
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": http.StatusBadRequest,
+			"msg":  "Now such option!",
+		})
+		return
+	}
+	if len(retChart) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": http.StatusBadRequest,
+			"msg":  "No data now!",
+		})
+		return
+	} else {
+		b, err := json.Marshal(retChart)
+		if err != nil {
+			fmt.Println("query result convert to json error!")
+			return
+		} else {
+			c.Writer.Write(b)
+			return
+		}
+	}
+}
+
+// historical data
+func historical_page(c *gin.Context) {
+	db := sqlInit()
+	coinName := c.Query("coinName")
+	if !db.HasTable("history-" + coinName) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": http.StatusBadRequest,
+			"msg":  "No query coin!",
+		})
+		return
+	}
+	tS := c.Query("timeStart")
+	tE := c.Query("timeEnd")
+	timeStart, _ := strconv.ParseInt(tS, 10, 64)
+	timeEnd, _ := strconv.ParseInt(tE, 10, 64)
+	tS = time.Unix(int64(timeStart), 0).Format("2006-01-02")
+	tE = time.Unix(int64(timeEnd), 0).Format("2006-01-02")
+	var his []CoinHistoricalQuote
+	// var hisList []string
+	db.Table("history-"+coinName).Where("time_open BETWEEN ? AND ?", tS, tE).Order("time_open").Find(&his)
+	if len(his) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": http.StatusBadRequest,
+			"msg":  "No data now!",
+		})
+		return
+	} else {
+		b, err := json.Marshal(his)
+		if err != nil {
+			fmt.Println("query result convert to json error!")
+		} else {
+			c.Writer.Write(b)
+			return
+		}
+	}
+}
