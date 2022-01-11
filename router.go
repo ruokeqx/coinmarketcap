9c5265b359f329ba6214d11fdf8a8bcbb40dbee5
ruokeqx <ruokeqx@163.com>
2022-01-11 14:37:11 +0800

    finish page offset

--- router.go
+++ router.go
@@ -408,7 +408,9 @@ func TokenAuthMiddleware(c *gin.Context) {
 func chart_page(c *gin.Context) {
 	// /data-api/v3/cryptocurrency/detail/chart?coinName=(?)&range=(?)&convertId=(?)
 	db := sqlInit()
-	coinName := c.Query("coinName")
+	coinName := c.PostForm("coinName")
+	pages, _ := strconv.Atoi(c.PostForm("pages"))
+	limits, _ := strconv.Atoi(c.PostForm("limits"))
 	if !db.HasTable("chart-" + coinName) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"code": http.StatusBadRequest,
@@ -416,61 +418,16 @@ func chart_page(c *gin.Context) {
 		})
 		return
 	}
-	queryRange := c.Query("range")
-	convertId := c.Query("convertId")
-	fmt.Println(convertId)
-	var tE int64
-	var tS int64
 	var chart []CoinPointQuote
-	var retChart []CoinPointQuote
-	switch queryRange {
-	case "1D":
-		// fmt.Println("1D")
-		tE = time.Now().Unix()
-		tS = tE - 60*60*24
-		db.Table("chart-"+coinName).Where("time BETWEEN ? AND ?", tS, tE).Order("time").Find(&chart)
-		retChart = chart
-	case "7D":
-		// fmt.Println("7D")
-		tE = time.Now().Unix()
-		tS = tE - 60*60*24*7
-		db.Table("chart-"+coinName).Where("time BETWEEN ? AND ?", tS, tE).Order("time").Find(&chart)
-		retChart = chart
-	case "1M":
-		// fmt.Println("1M")
-		tE = time.Now().Unix()
-		tS = tE - 60*60*24*30
-		db.Table("chart-"+coinName).Where("time BETWEEN ? AND ?", tS, tE).Order("time").Find(&chart)
-		for index, cont := range chart {
-			if index%12 == 0 {
-				retChart = append(retChart, cont)
-			}
-		}
-	case "1Y":
-		// fmt.Println("1Y")
-		tE = time.Now().Unix()
-		tS = tE - 60*60*24*365
-		db.Table("chart-"+coinName).Where("time BETWEEN ? AND ?", tS, tE).Order("time").Find(&chart)
-		for index, cont := range chart {
-			if index%288 == 0 {
-				retChart = append(retChart, cont)
-			}
-		}
-	default:
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code": http.StatusBadRequest,
-			"msg":  "Now such option!",
-		})
-		return
-	}
-	if len(retChart) == 0 {
+	db.Table("chart-" + coinName).Order("time").Offset((pages - 1) * limits).Limit(limits).Find(&chart)
+	if len(chart) == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"code": http.StatusBadRequest,
 			"msg":  "No data now!",
 		})
 		return
 	} else {
-		b, err := json.Marshal(retChart)
+		b, err := json.Marshal(chart)
 		if err != nil {
 			fmt.Println("query result convert to json error!")
 			return
@@ -484,7 +441,9 @@ func chart_page(c *gin.Context) {
 // historical data
 func historical_page(c *gin.Context) {
 	db := sqlInit()
-	coinName := c.Query("coinName")
+	coinName := c.PostForm("coinName")
+	pages, _ := strconv.Atoi(c.PostForm("pages"))
+	limits, _ := strconv.Atoi(c.PostForm("limits"))
 	if !db.HasTable("history-" + coinName) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"code": http.StatusBadRequest,
@@ -492,15 +451,8 @@ func historical_page(c *gin.Context) {
 		})
 		return
 	}
-	tS := c.Query("timeStart")
-	tE := c.Query("timeEnd")
-	timeStart, _ := strconv.ParseInt(tS, 10, 64)
-	timeEnd, _ := strconv.ParseInt(tE, 10, 64)
-	tS = time.Unix(int64(timeStart), 0).Format("2006-01-02")
-	tE = time.Unix(int64(timeEnd), 0).Format("2006-01-02")
 	var his []CoinHistoricalQuote
-	// var hisList []string
-	db.Table("history-"+coinName).Where("time_open BETWEEN ? AND ?", tS, tE).Order("time_open").Find(&his)
+	db.Table("history-" + coinName).Order("time_open").Offset((pages - 1) * limits).Limit(limits).Find(&his)
 	if len(his) == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"code": http.StatusBadRequest,
@@ -511,6 +463,7 @@ func historical_page(c *gin.Context) {
 		b, err := json.Marshal(his)
 		if err != nil {
 			fmt.Println("query result convert to json error!")
+			return
 		} else {
 			c.Writer.Write(b)
 			return
